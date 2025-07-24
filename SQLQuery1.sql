04087d3d31a4e509261dcd7d685673961bb84004
thegig009 <loveesesgig2005@gmail.com>
2025-07-24 09:34:34 +0700

    Add files via upload

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,3 +1,22 @@
 SELECT EmployeeID, TitleOfCourtesy, FirstName, LastName
 FROM Employees
 WHERE City = 'London';
+
+SELECT *
+FROM Products
+WHERE CategoryID IN (1, 2, 4, 8)
+  AND UnitPrice BETWEEN 50 AND 100;
+
+SELECT Country, City, CompanyName, ContactName, Phone
+FROM Customers;
+
+
+SELECT ProductID, ProductName, CategoryID, UnitPrice
+FROM Products
+WHERE (CategoryID = 1 AND UnitPrice <= 50)
+   OR (CategoryID = 8 AND UnitPrice <= 75);
+
+SELECT CompanyName
+FROM Customers
+WHERE Country = 'USA'
+  AND Fax IS NULL;
